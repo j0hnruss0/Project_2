b8f21ea0c54967b448432c2054afd083eff94882
Jonathan Behar <jdb443@stern.nyu.edu>
2019-08-03 15:32:58 -0400

    jon config fix

--- models/index.js
+++ models/index.js
@@ -5,11 +5,12 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
+var config = require(__dirname + "/../config/config.js")[env];
 
-console.log("WHAT ENV",env)
+console.log("WHAT ENV", env);
 var db = {};
 
+console.log("config", config);
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
