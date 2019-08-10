b76b32a49e1b4438d72d340c535dbd0b45efe19d
Austin T <austin.taranto@gmail.com>
2019-08-09 21:19:27 -0400

    have working fight for clikcing on certain characters

--- html files/battle-page/battle.js
+++ html files/battle-page/battle.js
@@ -1,38 +1,158 @@
-$( "#fight" ).click(function() {
-  $( "#captainmarvel" ).animate({
-    width: "60%",
-    opacity: 0.4,
-    marginLeft: "0.6in",
-    borderWidth: "10px"
-  }, 1500 );
-});
+// var selectedHeroes = [];
+function selectHeros(event) {
+  var heroes = $("input[type='radio']:checked");
+  // $(heroes).animate({
+  //   marginLeft: "2%",
+  // });
+  for(let i = 0; i< heroes.length;  i++){
+    if($(heroes[i]).hasClass('captainmarvel')){
+      captainmarvelAnimate();
 
-$( "#fight" ).click(function() {
-  $( "#batman" ).animate({
-    width: "60%",
-    opacity: 0.4,
-    marginLeft: "0.6in",
-    borderWidth: "10px"
-  }, 1500 );
-});
+    }else if($(heroes[i]).hasClass('superman')){
+      supermanAnimate();
+    }
+  }
 
-$( "#fight" ).click(function() {
-  $( "#ironman" ).animate({
-    width: "60%",
-    opacity: 0.4,
-    marginLeft: "0.6in",
-    borderWidth: "10px"
-  }, 1500 );
-});
+  function captainmarvelAnimate(){
+    $( ".captainmarvel" ).animate({
+      marginLeft: "15%",
+    });
+  }
 
-$( "#fight" ).click(function() {
-  $( "#deadpool" ).animate({
-    width: "60%",
-    opacity: 0.4,
-    marginLeft: "0.6in",
-    borderWidth: "10px"
-  }, 1500 );
-});
+  function supermanAnimate(){
+    $( ".superman" ).animate({
+      marginLeft: "25%",
+    });
+  }
+
+  for(let j = 0; j< heroes.length;  j++){
+    if($(heroes[j]).hasClass('batman')){
+      batmanAnimate();
+
+    }else if($(heroes[j]).hasClass('superman2')){
+      superman2Animate();
+    }
+  }
+
+  function batmanAnimate(){
+    $( ".batman" ).animate({
+      marginLeft: "10%",
+    });
+  }
+
+  function superman2Animate(){
+    $( ".superman2" ).animate({
+      marginLeft: "35%",
+    });
+  }
+
+  for(let e = 0; e< heroes.length;  e++){
+    if($(heroes[e]).hasClass('ironman')){
+      ironmanAnimate();
+
+    }else if($(heroes[e]).hasClass('blackpanther')){
+      blackpantherAnimate();
+    }
+  }
+
+  function ironmanAnimate(){
+    $( ".ironman" ).animate({
+      marginLeft: "32%",
+    });
+  }
+
+  function blackpantherAnimate(){
+    $( ".blackpanther" ).animate({
+      marginLeft: "20%",
+    });
+  }
+
+  for(let k = 0; k< heroes.length;  k++){
+    if($(heroes[k]).hasClass('deadpool')){
+      deadpoolAnimate();
+
+    }else if($(heroes[k]).hasClass('xmen')){
+      xmenAnimate();
+    }
+  }
+
+  function deadpoolAnimate(){
+    $( ".deadpool" ).animate({
+      marginLeft: "30%",
+    });
+  }
+
+  function xmenAnimate(){
+    $( ".xmen" ).animate({
+      marginLeft: "14%",
+    });
+  }
+  
+ 
+  /**
+   * read the input selection value
+   * add superhero selected to global array
+   * superhero and which side
+   */
+}
+
+// $( "#fight" ).click(function() {
+
+//   /** (input === 2)
+//    * checks if any superhero selected, at least 2. both should have same amount selected
+//    * if so, animate them
+//    * if not, display message that heros need to be selected
+//    */
+
+// }
+
+
+// $( "#fight" ).click(function(event) {
+//   selectHeros(event);
+  // $( "#captainmarvel" ).animate({
+  //   width: "60%",
+  //   opacity: 0.8,
+  //   marginLeft: "0.6in",
+  //   borderWidth: "10px"
+  // }, 1500 );
+// });
+
+$( "#fight" ).on('click', selectHeros);
+  
+  // $( "#captainmarvel" ).animate({
+  //   width: "60%",
+  //   opacity: 0.8,
+  //   marginLeft: "0.6in",
+  //   borderWidth: "10px"
+  // }, 1500 );
+// });
+
+// $( "#fight" ).click(function() {
+//   $( "#batman" ).animate({
+//     width: "80%",
+//     opacity: 0.6,
+//     marginLeft: "0.2in",
+//     borderWidth: "10px"
+//   }, 1500 );
+// });
+
+// $( "#fight" ).click(function() {
+//   $( "#ironman" ).animate({
+//     width: "50%",
+//     opacity: 0.7,
+//     marginLeft: "4.0in",
+//     borderWidth: "10px"
+//   }, 1500 );
+// });
+
+// $( "#fight" ).click(function() {
+//   $( "#deadpool" ).animate({
+//     width: "40%",
+//     opacity: 0.5,
+//     marginLeft: "1.5in",
+//     borderWidth: "10px"
+//   }, 1500 );
+// });
 
 // $( "#left" ).click(function() {
 //     $( "#Captain-Marvel" ).animate({
